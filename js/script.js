fabc06a4ecaae852161a51f130a1e60bd94dd2a8
Simone <cirino.simone99@gmail.com>
2022-05-24 16:20:56 +0200

    finito esercizio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const container = new Vue({
+    el : '#container',
+    data : {
+        string : 'prova di testo inserito in data',
+        stringClass : 'red',
+        image : 'img/image.png',
+        imageClass : 'img-size'
+    }
+});
